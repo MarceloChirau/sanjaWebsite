2d8cde56ff291b021d5abd91096765b0b3a9e2ba
marceloDev <marcelochirau@gmail.com>
2025-11-20 15:18:43 +0100

    added two btns,remains to make the hero responsive

--- script.js
+++ script.js
@@ -22,60 +22,95 @@ const p1=document.getElementById('p1');
 const p2=document.getElementById('p2');
 const imgSlide=document.getElementById('imgSlide');
 
-setTimeout(()=>{
-h2Title.textContent='Toperi';
-  
-  h2Title.classList.add('slideFromAbove');
-},1000);
+const heroObject=[{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/goodCakeTopper.PNG'
+},
+{
+    h2Title:'Boceri',
+    p1:'za rakja',
+    p2:'od 5€',
+    src:'imagesForHero/boca1.jpeg'
+},
+{
+    h2Title:'Etickete',
+    p1:'za spomene',
+    p2:'od 5€',
+    src:'imagesForHero/groblje6.jpeg'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/heski7.png'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/key2.png'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/stamps.png'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/trophy.jpeg'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/woodHeart.jpeg'
+},
+{
+    h2Title:'Toperi',
+    p1:'za torte',
+    p2:'od 5€',
+    src:'imagesForHero/yellowSign.jpeg'
+}
+
+
+
+]
+let animations=['slideFromAbove','slideFromLeft']
+let  currentIndex=0;
+
+function myHero(){
+let nextIndex=heroObject[currentIndex];
+
+
+
+h2Title.classList.remove(...animations);
+p1.classList.remove(...animations);
+p2.classList.remove(...animations);
+
+imgSlide.src=nextIndex.src;
+h2Title.textContent=nextIndex.h2Title;
+p1.textContent=nextIndex.p1;
+p2.textContent=nextIndex.p2;
+
+currentIndex=(currentIndex+1)%heroObject.length;
 
 setTimeout(()=>{
-    p1.textContent='za torte';
-    p1.classList.add('slideFromLeft');
-},2000);
-
-setTimeout(()=>{
-   p2.textContent='od 5€';
-   p2.classList.add('slideFromLeft');
-
     
-},3000);
-
-
-setTimeout(()=>{
-    h2Title.textContent='';
-    p1.textContent='';
-    p2.textContent='';
-    
-    h2Title.classList.remove('slideFromAbove');
-    p1.classList.remove('slideFromLeft');
-    p2.classList.remove('slideFromLeft');
-
-imgSlide.src='images/key2.png';
-
-
-},3500);
-
-
-setTimeout(()=>{
-h2Title.textContent='Kluce';
-h2Title.classList.add('slideFromAbove');
+        h2Title.classList.add(animations[0]);
+        p1.classList.add(animations[1]);
+        p2.classList.add(animations[1]);
     
 
+   
 
-},4000)
 
-setTimeout(()=>{
-    p1.textContent='za svaki';
-    p1.classList.add('slideFromLeft');
-        
-    
-    
-    },5000);
+},0);
 
-    setTimeout(()=>{
-        p2.textContent='vrata';
-        p2.classList.add('slideFromLeft');
-            
-        
-        
-        },6000);
+}
+myHero();
+setInterval(myHero,5000);
